38156b94915640a427671d98501d9ec0393823e2
KyleHu14 <lloydpearce4@gmail.com>
2023-05-10 22:16:43 -0700

    Added fonts

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,9 @@ import Image from "next/image";
 
 const Navbar = () => {
 	return (
+		// Entire Nav Container
 		<nav className={styles.navbar}>
+			{/* Left Part of Navbar */}
 			<div className={styles.navLeft}>
 				<Image
 					src="/uci-anteater.png"
@@ -20,21 +22,25 @@ const Navbar = () => {
 				</Link>
 			</div>
 
+			{/* Right Part of Navbar */}
 			<div className={styles.navRightLinks}>
 				<Link className={`${styles.navLink}`} href="/">
 					Home
 				</Link>
-				|
+				<div className={styles.linkDivider}>|</div>
 				<Link className={`${styles.navLink}`} href="/">
 					Course Ratings
 				</Link>
-				|
+				<div className={styles.linkDivider}>|</div>
 				<Link className={`${styles.navLink}`} href="/">
 					Course Recommendations
 				</Link>
 			</div>
-			<div className={styles.accountIcon}>
+
+			{/* Account Icon */}
+			<div className={styles.accountIconContainer}>
 				<Image
+					className={styles.accountIcon}
 					src="/account-icon.png"
 					width={60}
 					height={60}
@@ -44,5 +50,7 @@ const Navbar = () => {
 		</nav>
 	);
 };
+
+// <a href="https://www.flaticon.com/free-icons/profile" title="profile icons">Profile icons created by Freepik - Flaticon</a>
 
 export default Navbar;
